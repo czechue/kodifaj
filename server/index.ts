bea72b176faafa12a10b5d82189dd8b645b37930
Michal Lester <michal.lester@allegro.pl>
2019-10-04 23:47:00 +0200

    add headers

--- server/index.ts
+++ server/index.ts
@@ -11,6 +11,12 @@ const handle = app.getRequestHandler();
 
 app.prepare().then(() => {
   const server = express();
+  // Allows for cross origin domain request:
+  server.use(function (_req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*')
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
+    next()
+  })
 
   endpoints(server);
   routes(app, server);
