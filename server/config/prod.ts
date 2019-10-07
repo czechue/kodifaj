a8d592d6ec20ee01753dc919058525f44b31f59c
Michal Lester <michal.lester@allegro.pl>
2019-10-07 22:26:56 +0200

    13 | config prod

--- server/config/prod.ts
+++ server/config/prod.ts
@@ -6,7 +6,3 @@ export default {
   mongoURI: process.env.MONGO_URI,
   cookieKey: process.env.COOKIE_KEY
 } as Keys;
-
-// mongodb+srv://czechue:fender15@kodifaj-prod-72rrb.mongodb.net/anything?retryWrites=true&w=majority
-// 1084049677894-n7etsq7edrvcu0orgt1cpaeckoh4462e.apps.googleusercontent.com
-// -3NOhq_IJ6Ns_dHIvZeEQaXU
