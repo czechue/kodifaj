0f9378aacc9fd5c67ae24fa43434cff047b0f0f5
Michal Lester <michal.lester@allegro.pl>
2019-11-03 21:59:34 +0100

    23 | fix TS error

--- dummy/users.ts
+++ dummy/users.ts
@@ -2,13 +2,13 @@ import { UserProfil } from "../lib/interfaces";
 
 const users: UserProfil[] = [
   {
-    id: "5da6489aa330ec93594ffae6",
+    _id: "5da6489aa330ec93594ffae6",
     login: "czesław",
     photo: 'https://avatars1.githubusercontent.com/u/21274642?v=4',
     githubId: "19823401",
   },
   {
-    id: "9283472984293482",
+    _id: "9283472984293482",
     login: "2hjl",
     photo: 'https://avatars1.githubusercontent.com/u/21274642?v=4',
     githubId: "421123",
