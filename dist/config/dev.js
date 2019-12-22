fad6f8195e4afba2b13584bb5355239b72b86a0e
Michal Lester <michal.lester@allegro.pl>
2019-12-22 23:16:09 +0100

    build

--- dist/config/dev.js
+++ dist/config/dev.js
@@ -5,7 +5,7 @@ exports.default = {
     googleClientSecret: 'aWlB7bu891nFW7XgvQqWyGiP',
     githubClientID: 'Iv1.9b72f91938a2c402',
     githubClientSecret: '2ddfabd1e421af76cba67ef4b2082d83ab9c07b6',
-    mongoURI: 'mongodb+srv://czechue:fender15@kodifaj-gzt7a.mongodb.net/anything?retryWrites=true&w=majority',
+    mongoURI: 'mongodb://mo1272_kodifaj:Fender15!@195.167.159.159:27017/mo1272_kodifaj',
     cookieKey: '1i2hwiuashdiuahdUASI12QWASGU',
 };
 //# sourceMappingURL=dev.js.map
--- dist/config/prod.js
+++ dist/config/prod.js
@@ -5,8 +5,7 @@ exports.default = {
     googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
     githubClientID: process.env.GITHUB_CLIENT_ID,
     githubClientSecret: process.env.GITHUB_CLIENT_SECRET,
-    // mongoURI: process.env.MONGO_URI,
-    mongoURI: 'mongodb+srv://czechue:fender15@kodifaj-gzt7a.mongodb.net/anything?retryWrites=true&w=majority',
+    mongoURI: process.env.MONGO_URI,
     cookieKey: process.env.COOKIE_KEY,
 };
 //# sourceMappingURL=prod.js.map
