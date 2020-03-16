f603a502063a7d056f876be52668f21077931a72
Michal Lester <michal.lester@allegro.pl>
2020-03-16 22:31:21 +0100

    update dep

--- dist/api/tasks/dto/create-task-body.dto.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-class CreateTaskBodyDto {
-}
-exports.CreateTaskBodyDto = CreateTaskBodyDto;
-//# sourceMappingURL=create-task-body.dto.js.map
