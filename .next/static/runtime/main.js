7e80f29ac73f19030e0653106bdb463a47bdbd52
michal.lester87@gmail.com <michal.lester87@gmail.com>
2020-04-22 00:05:10 +0200

    remove dist and .next

--- .next/static/runtime/main.js
+++ .next/static/runtime/main.js
@@ -5157,7 +5157,7 @@ function connect(options) {
       error.name = err.name;
       error.stack = err.stack; // __NEXT_DIST_DIR is provided by webpack
 
-      (0, _sourceMapSupport.rewriteStacktrace)(error, "/Users/michal.lester/Documents/ML/kodifaj/.next");
+      (0, _sourceMapSupport.rewriteStacktrace)(error, "/home/czechue/Documents/kodifaj/.next");
       return error;
     }
   };
